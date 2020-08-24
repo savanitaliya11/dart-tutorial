3cc19f78b482fcd603345f6527f02295e00e7388
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2020-08-24 16:02:41 +0530

    for loop

---
+++ tut 5.1.dart
@@ -0,0 +1,23 @@
+void loop(){
+  //for loop
+/*
+  for (var i=0; i<4; i++){
+    print('hii');
+  }
+
+  //while loop
+
+  var i = 1;
+  while (i < 4){
+    print('hello');
+    i++;
+  }
+*/
+  //do.. while ..loop
+  var i = 1;
+  do{
+    print('how r u');
+    i++;
+  }while(i < 4);
+
+}
