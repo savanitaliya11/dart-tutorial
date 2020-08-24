f791965a3a3334331f5c3647603fe6269b2ef8d9
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2020-08-24 16:00:50 +0530

    final and cons keyword

---
+++ tut 3.3.dart
@@ -0,0 +1,18 @@
+void tut3(){
+
+  //Final keyword
+  final name = 'savan';
+  print(name);
+
+  //cons keyword
+  const PI = 3.14;
+  print(PI);
+
+}
+
+class circle{
+  final surname = 'xyz';
+  static const gravity = 3.8;
+
+}
+
