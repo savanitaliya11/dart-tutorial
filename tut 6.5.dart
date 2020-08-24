728aaa74a3b2a60c79c14b0449e099e79a05c005
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2020-08-24 16:05:31 +0530

    named parameter

---
+++ tut 6.5.dart
@@ -0,0 +1,16 @@
+
+void main(){
+ // xyz(1, height: 2, weight: 3); you can change position of value of your parameter
+  xyz(1, weight: 2, height: 4);
+
+}
+
+//Named parameter
+
+void xyz(int length, {int height, int weight}){
+  print('length is $length');
+  print('height is $height');
+  print('weight is $weight');
+  print('volume is ${length * height * weight}');
+
+}
