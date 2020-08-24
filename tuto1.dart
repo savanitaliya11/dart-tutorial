6270be6f4dbc040a170c5bf938faaedc1004be75
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2020-08-24 15:59:29 +0530

    Basic of dart

---
+++ tuto1.dart
@@ -0,0 +1,20 @@
+void tut1(){
+
+int i = 2;  //Number = int
+//datatype varname = var value
+
+
+String name = 'savan';  //literals
+String s1 = 'single';
+String s2 = "Double";
+String s3 = 'It\'s easy';
+String s4 = "It;s easy";
+print(s4);
+
+print('My name is $name');  //String interpolations
+int a = 1;
+int b = 3;
+print('sum of $a and $b is ${a+b}');
+print(name);
+print('the size of $name is ${name.length}');
+}
