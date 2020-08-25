b8345e3ce9a39c83413301a84724723b4c5d7d14
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2020-08-25 12:31:02 +0530

    Interface

---
+++ tut 9.7.dart
@@ -0,0 +1,38 @@
+//Interface
+//interface is used when you need to concrete implementations of all
+//of it's functions within it's sub class.
+void main(){
+  var tea = Teachers();
+  tea.marks();
+  print(' ');
+  tea.work();
+  //var stu = Student();
+  //stu.marks();
+}
+
+class Student{
+  void marks(){
+    print('marks of students');
+  }
+}
+
+class peun{
+  void work(){
+    print('peon has a work point');
+  }
+}
+
+//you can interface class by implements.
+//you can implements more than one class at a time with implementations you
+//can use more than one class using extends.
+//but you have to define it's method it's mandatory.
+class Teachers implements Student, peun{
+
+  void work(){
+    print('this is peon');
+  }
+
+  void marks() {
+    print('points of teachers');
+  }
+}
