c4f74cbbe859185a4e23e1d7b055fa536366b426
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2020-08-26 11:33:29 +0530

    Set

--- tut 11.3.dart
+++ tut 11.3.dart
@@ -1,17 +1,39 @@
-//Map
+//Set : Unordered index
+
 void main() {
-  Map<dynamic, dynamic> name = {0: '239r', 1: "jreg", 2: 76, "3": '239r'};
+  //this set contain only String values
+  Set<String> name = Set();
+  name.add('savan'); //Store value with this operation.
 
-  name.myEach((k, v) => print("$k => $v"));
-  //name.forEach((key, value) {});
+  //Create a set from list
+  Set<int> num = Set.from([1, 2, 3]);
+
+  //Dynamic type set It can store all type's of value.
+  Set<dynamic> surname = Set();
+  surname.add(54);
+  surname.add('savan');
+
+  //Object type set It can also store all type's of values.
+  Set<Object> teacher = Set();
+  teacher.add('sdf');
+  teacher.add(5);
+
+  //Var type set that means it can allocate their type automatically.
+  var student = Set();
+  student.add(5);
+  student.add('savan');
+
+  Set<dynamic> ff = {'grg'};
+  ff.add('fdf');
+  print(ff);
+
+  student.forEach((element) => print(element));
   print(' ');
-  name.forEach((key, value) => print('$key => $value'));
-}
-
-extension on Map {
-  myEach(void f(k, v)) {
-    for (int i = 0; i < this.length; i++) {
-      f(this.keys.toList()[i], this.values.toList()[i]);
-    }
-  }
+  teacher.forEach((element) => print(element));
+  print(' ');
+  surname.forEach((element) => print(element));
+  print(' ');
+  num.forEach((element) => print(element));
+  print(' ');
+  name.forEach((element) => print(element));
 }
