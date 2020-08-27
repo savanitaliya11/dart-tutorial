689eb947d50cb1cfeddf29d20a46203f32873a82
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2020-08-27 10:32:19 +0530

    static

--- tut 3.3.dart
+++ tut 3.3.dart
@@ -1,18 +1,17 @@
-void tut3(){
-
+void tut3() {
   //Final keyword
   final name = 'savan';
   print(name);
+  //name = 'jigar';     //It will shows an error because name has a final property.
 
   //cons keyword
   const PI = 3.14;
   print(PI);
-
+  //PI = 21.4;  //It will shows an error
 }
 
-class circle{
+class circle {
   final surname = 'xyz';
   static const gravity = 3.8;
-
+  String name = 'abc';
 }
-
