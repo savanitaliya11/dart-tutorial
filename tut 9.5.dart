34b4750a59146e7e0ac1951cd10627f8166c1f8d
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2020-08-28 13:21:28 +0530

    inheritance constructor

--- tut 9.5.dart
+++ tut 9.5.dart
@@ -1,6 +1,5 @@
 //Inheritance constructor
-
-void main(){
+void main() {
   var dog = Dog('abc', 'white');
   print(' ');
 
@@ -9,25 +8,25 @@ void main(){
 
   var dog2 = Dog.myAnother();
   print(' ');
-
 }
-class Animal{
-  Animal(String color){
+
+class Animal {
+  Animal(String color) {
     print('Animal constructor');
   }
 
-  Animal.named(String breed){
+  Animal.named(String breed) {
     print('animal named constructor');
   }
 }
-class Dog extends Animal{
 
-  Dog(String breed, String color) : super.named('jf'){        //super keyword is by default there
+class Dog extends Animal {
+  Dog(String breed, String color) : super.named('jf') {
+    //super keyword is by default there
     print('Dog constructor');
-}
+  }
 
-
-  Dog.myAnother() : super.named('abf'){
-      print('my name constructor');
+  Dog.myAnother() : super.named('abf') {
+    print('my name constructor');
   }
 }
