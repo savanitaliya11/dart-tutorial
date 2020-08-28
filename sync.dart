61bd52d00c1e664be24d7a02a2d458efd1894b47
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2020-08-28 13:18:03 +0530

    Sync

---
+++ sync.dart
@@ -0,0 +1,17 @@
+/*Sync : for single value
+It will return single value like a normal function.
+*/
+
+void main() {
+  print(demo());
+  print(name());
+}
+
+//Sync funtion for single value
+int demo() {
+  return 10;
+}
+
+String name() {
+  return 'savan';
+}
