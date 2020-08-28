2be258e3ef2c88bb05610a44bf08a12799bdc809
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2020-08-28 13:22:24 +0530

    lambda expression

--- tut 10.1.dart
+++ tut 10.1.dart
@@ -1,32 +1,30 @@
 //Lambda expression
-void main(){
-
- // Student(2, 5);  //Normal function calling
+void main() {
+  // Student(2, 5);  //Normal function calling
 //1st way.
-   Function lam = (int i, int j){
+  Function lam = (int i, int j) {
     print('${i + j}');
   };
 //2nd way
-   var multi = (int num){
-     return num * 4;
-   };
+  var multi = (int num) {
+    return num * 4;
+  };
 
 //Short hand syntax
 
   Function stu = (int a, int b) => print('${a + b}');
   var percentage = (double f) => f / 500.0 * 100.0;
-   //Calling lambda function
-  lam(1 , 2);
+  //Calling lambda function
+  lam(1, 2);
   print(multi(5));
 
   //short hand calling
-  stu(4,5);
+  stu(4, 5);
   print(percentage(480.0));
-
 }
 
 //Normal function
-void Student(int i, int j){
+void Student(int i, int j) {
   var sum = i + j;
   print('$sum');
 }
