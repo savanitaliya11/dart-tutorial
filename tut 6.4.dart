3ffeafaa02d8e02d879abaebe1d2ca3572f27c6c
savanitaliya11 <40891740+savanitaliya11@users.noreply.github.com>
2020-08-28 13:20:03 +0530

    parameter type

--- tut 6.4.dart
+++ tut 6.4.dart
@@ -1,13 +1,12 @@
-void main(){
-  name('savan', 'ram', 'jigar');//All parameter values must be define
-  //Incase you haven't write then it will rise an error.
-  surname('Italiya','Dhameliya','Patel'); //last two surnames are optional
-  //Incase you haven't write then program will evaluate properly.
-
-
+void main() {
+  name('savan', 'ram', 'jigar'); //All parameter values must be define
+  //In case you haven't write then it will rise an error.
+  surname('Italiya', 'Dhameliya', 'Patel'); //last two surnames are optional
+  //In case you haven't write then program will evaluate properly in Optional required parameter.
 }
+
 //Required parameter
-void name(String name1, String name2, String name3){
+void name(String name1, String name2, String name3) {
   print('name is $name1');
   print('name is $name2');
   print('name is $name3');
@@ -15,8 +14,7 @@ void name(String name1, String name2, String name3){
 
 //Optional required parameter
 
-void surname(String name1, [String name2, String name3])
-{
+void surname(String name1, [String name2, String name3]) {
   print('surname is $name1');
   print('surname is $name2');
   print('surname is $name3');
